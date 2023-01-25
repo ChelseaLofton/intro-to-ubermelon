10f4331af63a7a806fd92415fdbebd40504f5d37
Chelsea <113374977+ChelseaLofton@users.noreply.github.com>
2023-01-24 18:59:00 -0800

    corrected comments

--- process.py
+++ process.py
@@ -1,12 +1,12 @@
 log_file = open("um-server-01.txt")
 """stores to a variable the file containing sales data"""
 
-def generate_sales_reports(log_file): """defining the function"""
-    for line in log_file:             """goes through each line in the file"""
-        line = line.rstrip()          """removes unecessary empty space"""
-        day = line[0:3]               """for each line in log, slices the first three and assigns to day"""
-        if day == "Mon":              """if those characters = mon"""
-            print(line)               """prints corresponding line"""
+def generate_sales_reports(log_file):  # defining the function
+    for line in log_file:             # goes through each line in the file"""
+        line = line.rstrip()          # removes unecessary empty space"""
+        day = line[0:3]               # for each line in log, slices the first three and assigns to day"""
+        if day == "Mon":              # if those characters = mon"""
+            print(line)               # prints corresponding line"""
 
 
-generate_sales_reports(log_file)      """calls function passing in log_file as an argument"""
+generate_sales_reports(log_file)      # calls function passing in log_file as an argument"""
